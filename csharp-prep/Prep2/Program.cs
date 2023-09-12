86e97b0ad58a93c1d491de34f7aab27a44bb5724
britty042 <brittney.law@gmail.com>
2023-09-11 21:42:58 -0600

    Finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+        
+        string grade = "";
+
+        if (number >= 90)
+        {
+            grade = "A"; 
+        }
+        else if (number >= 80)
+        {
+            grade = "B"; 
+        }
+        else if (number >= 70)
+        {
+            grade = "C";         
+        }
+        else if (number >= 60)
+        {
+            grade = "D";         
+        }
+        else
+        {
+            grade = "F";         
+        }
+        Console.WriteLine($"This is your grade: {grade}");
+
+        if (number > 70)
+        {
+            Console.WriteLine($"Congrats!  You passed!");
+        }
+        else
+        {
+            Console.WriteLine($"Don't quit!  You'll do better next time!");
+        }
     }
 }
