5bd0999ffba25ae75c178431c7e719d1bf6df4a6
britty042 <brittney.law@gmail.com>
2023-10-04 20:37:14 -0600

    Completed W04 Learning Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assign1 = new Assignment("Brittney", "Math");
+        Console.WriteLine(assign1.GetSummary());
+
+        MathAssignment mathassign1 = new MathAssignment("Brittney", "Math", "Section 7.3", "8-19");
+        Console.WriteLine(mathassign1.GetSummary());
+        Console.WriteLine(mathassign1.GetHomeworkList());
     }
 }
